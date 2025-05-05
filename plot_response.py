36b19c1339c6cc12c5e308b8b6114ff0de2a1fe3
pltaylor16 <peterllewelyntaylor@gmail.com>
2025-05-05 17:14:12 -0400

    see last

--- plot_response.py
+++ plot_response.py
@@ -58,7 +58,7 @@ common_kwargs = dict(
 
 # Run for two alpha values (beta fixed)
 alpha_data = {}
-for alpha in [0.5, 1.5]:
+for alpha in [0.9, 1.1]:
     sim = ProcessMaps(alpha=alpha, beta=1.0, **common_kwargs)
     maps = sim.generate_noisy_kappa_maps()
     dv = sim.compute_data_vector(maps)
@@ -78,7 +78,7 @@ print("Saved alpha plot to data/alpha.png")
 
 # Run for two beta values (alpha fixed)
 beta_data = {}
-for beta in [0.5, 1.5]:
+for beta in [0.9, 1.1]:
     sim = ProcessMaps(alpha=1.0, beta=beta, **common_kwargs)
     maps = sim.generate_noisy_kappa_maps()
     dv = sim.compute_data_vector(maps)
