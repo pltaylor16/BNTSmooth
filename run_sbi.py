f14621ef7b773f81116a78e9affdeebe7df6da2e
pltaylor16 <peterllewelyntaylor@gmail.com>
2025-05-07 09:02:19 -0400

    added an embedding net for dimensional reduction

--- run_sbi.py
+++ run_sbi.py
@@ -6,7 +6,7 @@ from sbi import utils as sbi_utils
 from sbi import inference as sbi_inference
 import torch
 from getdist import MCSamples, plots
-
+import torch.nn as nn
 
 def parent_nz(z):
     z_euc = 0.9 / 2 ** 0.5
@@ -47,7 +47,7 @@ def make_equal_ngal_bins(nz_func, z_grid, nbins, sigma_z0=0.05):
 nside = 512
 l_max = 1500
 nslices = 15
-n_simulations_per_round = [200,500,500,1000]
+n_simulations_per_round = [200,200,200,500]
 n_rounds = len(n_simulations_per_round)
 
 nbins = 5
@@ -89,11 +89,24 @@ def worker(theta):
 
 
 def train_density_estimator(theta, x, prior, x_obs, n_samples):
-    inference = sbi_inference.SNPE(prior=prior, density_estimator="mdn")
+    import torch.nn as nn
+
+    embedding_net = nn.Sequential(
+        nn.Linear(x.shape[1], 2),
+        nn.ReLU(),
+    )
+
+    inference = sbi_inference.SNPE(
+        prior=prior,
+        density_estimator="mdn",
+        embedding_net=embedding_net
+    )
+
     density_estimator = inference.append_simulations(theta, x).train()
     posterior = inference.build_posterior(density_estimator)
     samples = posterior.sample((n_samples,), x=x_obs)
     return posterior, samples
+
 
 
 def main():
