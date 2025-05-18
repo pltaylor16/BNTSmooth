26794dc9b77ac71fa8c5bb1eb0056012558d6518
pltaylor16 <peterllewelyntaylor@gmail.com>
2025-05-18 14:41:20 -0400

    I need more fine graining for smoothing

--- compute_dvecs.py
+++ compute_dvecs.py
@@ -116,7 +116,7 @@ def main():
     args = parser.parse_args()
 
     Path("data").mkdir(exist_ok=True)
-    for smooth_mpc in [1., 3., 5., 10., 20.]:
+    for smooth_mpc in [7., 13., 16.]:
         compute_dvecs(
             base_dir="/srv/scratch2/taylor.4264/BNTSmooth_data/maps/maps",
             baryon_feedback=7.0,
